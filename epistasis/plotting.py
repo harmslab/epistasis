7eff4e53ac8ff08d3c1c0f5144ba152e51adda73
Zachary Sailer <zachsailer@gmail.com>
2016-01-01 16:18:57 -0800

    remove some plotting and add new plotting

--- epistasis/plotting.py
+++ epistasis/plotting.py
@@ -9,149 +9,93 @@ from scipy.stats import norm as scipy_norm
 # Epistasis Graphing
 # ---------------------------------------------------
 
-def epistasis_bar(epistasis_map, sigmas=0, title="Epistatic interactions", string_labels=False, ax=None, color='b', figsize=[6,4]):
-    """ Plot the interactions sorted by their order.
-
-    Parameters:
-    ----------
-    title: str
-        The title for the plot.
-    sigmas:
-        Number of sigmas to represent the errorbars. If 0, no error bars will be included.
+def magnitude_vs_order(model, keep_sign=False, 
+        marker="o", 
+        color="b",
+        linestyle="None",
+        xlabel="Order", 
+        ylabel="Magnitude", 
+        title="", 
+        figsize=(6,4),
+        **kwargs):
     """
-    em = epistasis_map
-    if ax is None:
-        fig, ax = plt.subplots(1,1, figsize=figsize)
-    else:
-        fig = ax.get_figure()
+        Generate a plot of magnitude versus order.
+    
+    """    
+    orders = range(1, model.length+1)
+    magnitudes = []
+    for i in orders:
+        coeffs = np.array(list(model.Interactions.get_order(i).values()))
+        
+        # Do we care about signs? 
+        if keep_sign is False:
+            coeffs = abs(coeffs)
+        
+        # Add magnitudes
+        magnitudes.append(np.mean(coeffs))
+    
+    # Initialize the figure and axis
+    fig, ax = plt.subplots(figsize=figsize)
+    
+    ybounds = int(max(abs(np.array(magnitudes))))+1
+    
+    # Create plot
+    ax.plot(orders, magnitudes, marker=marker, color=color, linestyle=linestyle, **kwargs)
+    ax.axis([orders[0], orders[-1], -ybounds, ybounds])
+    
+    if keep_sign:
+        ax.hlines(0, orders[0], orders[-1], linestyle="--")
+    
+    # ---------------------------------
+    # Plot styling
+    # ---------------------------------
+    
+    extra_limit_frac = 0.05
+    spine_widths = 1.35
+    line_widths = 1.5
+    
+    # Get current axis limits
+    xlimits = list(ax.get_xlim())
+    ylimits = list(ax.get_ylim())
+    xticks = list(ax.get_xticks())
+    yticks = list(ax.get_yticks())
 
-    y = em.Interactions.values
-    if string_labels is True:
-        xtick = em.Interactions.genotypes
-    else:
-        xtick = em.Interactions.keys
-        xlabel = "Interaction Indices"
+    # Extend the graph by 5 percent on all sides
+    xextra = extra_limit_frac*(xlimits[1] - xlimits[0])
+    yextra = extra_limit_frac*(ylimits[1] - ylimits[0])
 
-    # plot error if sigmas are given.
-    if sigmas == 0:
-        ax.bar(range(len(y)), y, 0.9, alpha=0.4, align="center", color=color) #, **kwargs)
-    else:
-        yerr = em.Interactions.errors
-        ax.bar(range(len(y)), y, 0.9, yerr=sigmas*yerr, alpha=0.4, align="center", color=color) #,**kwargs)
+    # set ticks and tick labels
+    ax.set_xlim(xlimits[0] - xextra, xlimits[1] + xextra)
+    ax.set_ylim(ylimits[0] - yextra, ylimits[1] + yextra)
 
-    # vertically label each interaction by their index
-    plt.xticks(range(len(y)), np.array(xtick), rotation="vertical", family='monospace',fontsize=7)
-    ax.set_ylabel("Interaction Value", fontsize=14)
-    try:
-        ax.set_xlabel(xlabel, fontsize=14)
-    except:
-        pass
-    ax.set_title(title, fontsize=12)
-    ax.axis([-.5, len(y)-.5, -max(abs(y)), max(abs(y))])
-    ax.hlines(0,0,len(y), linestyles="dashed")
-    return fig, ax
+    # Remove right and top spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
 
-def epistasis_barh(epistasis_map, sigmas=0, title="Epistatic interactions",
-                    string_labels=False, ax=None, color='b', figsize=[6,4], partition=False):
-    """ Plot the interactions sorted by their order.
+    # Set the bounds for visible axes
+    ax.spines['bottom'].set_bounds(xlimits[0], xlimits[1])
+    ax.spines['left'].set_bounds(ylimits[0], ylimits[1])
 
-    Parameters:
-    ----------
-    title: str
-        The title for the plot.
-    sigmas:
-        Number of sigmas to represent the errorbars. If 0, no error bars will be included.
-    """
-    em = epistasis_map
-    if ax is None:
-        fig, ax = plt.subplots(1,1, figsize=figsize)
-    else:
-        fig = ax.get_figure()
+    # Thicken the spines
+    ax.spines['bottom'].set_linewidth(spine_widths)
+    ax.spines['left'].set_linewidth(spine_widths)
 
-    x = em.Interactions.values
-    if string_labels is True:
-        ytick = em.Interactions.genotypes
-        ylabel = "Interactions"
-    else:
-        ytick = em.Interactions.keys
-        ylabel = "Interaction Indices"
+    # Only show ticks on the left and bottom spines
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
 
-    # plot error if sigmas are given.
-    if sigmas == 0:
-        ax.barh(-np.arange(len(x)), x, 0.9, alpha=0.4, align="center", color=color) #, **kwargs)
-    else:
-        xerr = em.Interactions.errors
-        ax.barh(-np.arange(len(x)), x, 0.9, xerr=sigmas*xerr, alpha=0.4, align="center", color=color) #,**kwargs)
+    # Make ticks face outward and thicken them
+    ax.tick_params(direction='out', width=spine_widths)
 
-    # vertically label each interaction by their index
-    plt.yticks(-np.arange(len(x)), np.array(ylabels), rotation="horizontal", family='monospace', fontsize=7)
-    ax.set_xlabel("Interaction Value", fontsize=16, fontname='monospace')
-    ax.set_ylabel(ylabel, fontsize=16)
-    ax.set_title(title, fontsize=20)
-    ax.axis([-max(abs(x)), max(abs(x)), -len(x)+.5, .5])
-    ax.vlines(0,0,-len(x), linestyles="dashed")
-    return fig, ax
+    if xticks[-1] > xlimits[1]:
+        xticks = xticks[:-1]
+       
+    if yticks[-1] > ylimits[1]:
+        yticks = yticks[:-1]
+    
+    ax.set_xticks(xticks)
+    ax.set_yticks(yticks)
 
-def epistasis_bar_charts(em, sigmas=0, x_axis=[-1.5,1.5]):
-    """ Plot each order of epistasis from model as barcharts subplots.
-
-    Parameters:
-    ----------
-    em: EpistasisModel object
-        Must be a complete, fitted epistasis map
-    order: int
-        Highest order of epistasis to plot
-    """
-    length = em.length
-    order = em.order
-    fig, ax = plt.subplots(order, 1, figsize=[5,2*order])
-
-    for o in range(1, order+1):
-        interactions, errors = em.get_order(o, errors=sigmas)
-        labels = list(interactions.keys())
-        values = list(interactions.values())
-        n_terms = len(values)
-        index = np.arange(n_terms)
-        bar_width = .8
-        opacity = 0.5
-        rects1 = ax[o-1].barh(index, values, bar_width,
-                         alpha=opacity,
-                         color='b',
-                         xerr=sigmas*np.array(list(errors.values())),
-                        align="center",
-                        ecolor='r')
-        ticks = ax[o-1].set_yticklabels(labels, rotation="horizontal", fontsize=8)
-        ax[o-1].set_yticks(index)
-
-        axis_dim = list(x_axis) + [-1,len(index)]
-        ax[o-1].axis(axis_dim)
-        ax[o-1].vlines(0,-1,len(index), linestyles=u'solid')
-        if o != order:
-            ax[o-1].get_xaxis().set_ticks([])
-    ax[0].set_title("Order of Interaction: " +str(order))
-
-def ensemble_bar(ensemble, title="Ensemble Epistasis"):
-    """ Return a bar chart of ensemble interactions from an ensemble model calculation. """
-    fig, ax = plt.subplots(1,1, figsize=[12,6])
-    averages, deviations = ensemble.ensemble_averages()
-    xlabels = list()
-    y = list()
-    yerr = list()
-    for key in averages:
-        y.append(averages[key])
-        yerr.append(deviations[key])
-        xlabels.append(key)
-
-    ax.bar(range(len(y)), y, 0.9, yerr=yerr, alpha=0.4, align="center") #,**kwargs)
-
-    # vertically label each interaction by their index
-    plt.xticks(range(len(y)), np.array(xlabels), rotation="vertical")
-    ax.set_xlabel("Interaction term", fontsize=16)
-    ax.set_ylabel("Interaction Value", fontsize=16)
-    ax.set_title(title, fontsize=20)
-    ax.axis("tight")
-    ax.hlines(0,0,len(y), linestyles="dashed")
-    return fig, ax
 
 
 def bar_with_xbox(model,
