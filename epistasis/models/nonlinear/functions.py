0273909257f36b1e20c9ae49e21ee26e74b291a2
Zachary Sailer <zachsailer@gmail.com>
2016-08-31 13:21:29 -0700

    fix missing import in numpy function

--- epistasis/models/nonlinear/functions.py
+++ epistasis/models/nonlinear/functions.py
@@ -1,10 +1,11 @@
 import scipy
+import numpy
 
 def power_transform(x, lmbda, A, B):
     """Power transformation function."""
     gmean = scipy.stats.mstats.gmean(x + A)
     if lmbda == 0:
-        return gmean*np.log(x+A)
+        return gmean*numpy.log(x+A)
     else:
         first = (x+A)**lmbda
         out = (first - 1.0)/(lmbda * gmean**(lmbda-1)) + B
