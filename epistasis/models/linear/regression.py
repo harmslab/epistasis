49e607e1046dec8d26179ff25098c42854b14860
Zachary Sailer <zachsailer@gmail.com>
2016-12-13 13:47:17 -0800

    update linear regression class

--- epistasis/models/linear/regression.py
+++ epistasis/models/linear/regression.py
@@ -17,9 +17,9 @@ class EpistasisLinearRegression(_LinearRegression, _BaseModel):
         self.set_params(model_type=model_type, order=order, **kwargs)
 
     @X_fitter
-    def fit(self, X=None, y=None):
+    def fit(self, X=None, y=None, sample_weight=None):
         # Build input linear regression.
-        super(self.__class__, self).fit(X,y)
+        super(self.__class__, self).fit(X, y, sample_weight)
 
     @X_predictor
     def predict(self, X=None):
@@ -28,3 +28,23 @@ class EpistasisLinearRegression(_LinearRegression, _BaseModel):
     @X_fitter
     def score(self, X=None, y=None):
         return super(self.__class__, self).score(X, y)
+
+    @X_fitter
+    def _sample_fit(self, X=None, y=None):
+        """Sample the `fit` method from phenotype standard deviations."""
+        # Fit a model
+        y = self.gpm.stdeviations * _np.random.randn() + self.gpm.phenotypes
+        model = self.__class__
+        model.fit(X=X, y=y)
+        return model.coef_
+
+    @X_fitter
+    def _sample_predict(self, X=None):
+        """Sample the `predict` method from phenotype standard deviations."""
+        # Fit a model
+        y = self.gpm.stdeviations * _np.random.randn() + self.gpm.phenotypes
+        model = self.__class__
+        model.fit(X=X, y=y)
+        # predict from that model
+        predictions = model.predict(X=None)
+        return predictions
