ab0969e9e231a5869fd983e92c7da362a0f3a555
Zachary Sailer <zachsailer@gmail.com>
2017-02-14 16:04:25 -0800

    add bounds to power fit

--- epistasis/models/nonlinear/power.py
+++ epistasis/models/nonlinear/power.py
@@ -2,8 +2,12 @@ import scipy
 import numpy as np
 import inspect
 import json
+from scipy.optimize import curve_fit
 from .regression import EpistasisNonlinearRegression
 from epistasis.stats import gmean
+from ..base import X_fitter
+
+
 
 def power_transform(x, lmbda, A, B):
     """Power transformation function. Ignore zeros in gmean calculation"""
@@ -58,3 +62,29 @@ class EpistasisPowerTransform(EpistasisNonlinearRegression):
         params.update(gmean=self.gmean)
         with open(filename, "w") as f:
             json.dump(params, f)
+
+    @X_fitter
+    def _fit_(self, X=None, y=None, sample_weight=None, **kwargs):
+        """Fit the genotype-phenotype map for epistasis.
+        """
+        # Fit coeffs
+        guess = self._guess_model(X, y, sample_weight=sample_weight)
+        self.coef_ = guess.coef_
+        # Get the scale of the map.
+        x = guess.predict(X)
+        # Construct an array of guesses, using the scale specified by user.
+        guess = np.ones(self.parameters.n)
+        # Add model's extra parameter guesses to input array
+        for kw in kwargs:
+            index = self.parameters._mapping[kw]
+            guess[index] = kwargs[kw]
+        # create a sigma array for weighted fits with curve fit.
+        if sample_weight is None:
+            sigma = None
+        else:
+            sigma = 1 / np.sqrt(sample_weight)
+        # Curve fit the data using a nonlinear least squares fit
+        popt, pcov = curve_fit(self._function, x, y, p0=guess, sigma=sigma,
+            bounds=([-np.inf, -np.inf, -np.inf],[np.inf, np.inf, min(self.gpm.phenotypes)]))
+        for i in range(0, self.parameters.n):
+            self.parameters._set_param(i, popt[i])
